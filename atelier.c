878ab9f464ecb8a21e179defc85a02dcfc893781
Antoine Lamielle <antoine.lamielle@0x010c.fr>
2014-11-29 17:12:22 +0100

    Ajout des licences au début des sources

--- atelier.c
+++ atelier.c
@@ -1,3 +1,29 @@
+/*
+	PROGRAMME :
+		PeliKanban
+	DEVELOPPEUR :
+		Paul-Émile Bretegnier <paul-emile.bretegnier@utbm.fr>
+		Antoine LAMIELLE <antoine.lamielle@utbm.fr>
+	PROJECT START DATE :
+		November, 29th 2014
+	LICENCE :
+		PeliKanban is a project realised as a part of the LO41-lessons at the UTBM school, dealing with interprocess communication.
+
+		This program is free software; you can redistribute it and/or modify
+		it under the terms of the GNU General Public License as published by
+		the Free Software Foundation; either version 3 of the License, or
+		(at your option) any later version.
+
+		This program is distributed in the hope that it will be useful,
+		but WITHOUT ANY WARRANTY; without even the implied warranty of
+		MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+		GNU General Public License for more details.
+
+		You should have received a copy of the GNU General Public License along
+		with this program; if not, write to the Free Software Foundation, Inc.,
+		51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
 #include <stdlib.h>
 #include <stdio.>
 
